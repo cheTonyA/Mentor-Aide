a11dfcfc63c13e99ef048da34f0de978fde32f9d
tony <t_che@outlook.com>
2021-07-14 09:39:34 -0400

    Initial Commit

---
+++ Mentor-Aide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mentor-Aide
+//
+//  Created by Tony Che on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
